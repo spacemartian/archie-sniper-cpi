e07e046ee317fdd07b32d343be9cb463c8fa5caf
spacemartian <espacellama@gmail.com>
2024-11-21 14:02:22 +0000

    Added pump_fun_sell_token and renamed tip amount parameters

--- programs/archie-cpi/src/lib.rs
+++ programs/archie-cpi/src/lib.rs
@@ -1,6 +1,7 @@
 use anchor_lang::prelude::*;
 
 pub mod instructions;
+pub mod constants;
 
 use crate::instructions::*;
 
@@ -10,15 +11,19 @@ declare_id!("81JFbU5xub73UmkJFgUG2KTDLvtx9o7Kq8iK3hJFnEn9");
 pub mod pump {
     use super::*;
 
-    pub fn raydium_swap_token_in(ctx: Context<SwapTokens>, amount_in: u64, minimum_amount_out: u64, tip_amount: u64) -> Result<()> {
-        instructions::swap_exact_in(ctx, amount_in, minimum_amount_out, tip_amount)
+    pub fn raydium_swap_token_in(ctx: Context<SwapTokens>, amount_in: u64, minimum_amount_out: u64, jito_tip_sol: f64) -> Result<()> {
+        instructions::swap_exact_in(ctx, amount_in, minimum_amount_out, jito_tip_sol)
     }
 
-    pub fn raydium_swap_token_out(ctx: Context<SwapTokens>, max_amount_in: u64, amount_out: u64, tip_amount: u64) -> Result<()> {
-        instructions::swap_exact_out(ctx, max_amount_in, amount_out, tip_amount)
+    pub fn raydium_swap_token_out(ctx: Context<SwapTokens>, max_amount_in: u64, amount_out: u64, jito_tip_sol: f64) -> Result<()> {
+        instructions::swap_exact_out(ctx, max_amount_in, amount_out, jito_tip_sol)
     }
 
-    pub fn pump_fun_buy_token(ctx: Context<BuyPumpToken>, amount_sol: f64, slippage: f64) -> Result<()> {
-        instructions::buy_pump_tokens(ctx, amount_sol, slippage)
+    pub fn pump_fun_buy_token(ctx: Context<BuyPumpToken>, amount_sol: f64, slippage: f64, jito_tip_sol: f64) -> Result<()> {
+        instructions::buy_pump_tokens(ctx, amount_sol, slippage, jito_tip_sol)
+    }
+
+    pub fn pump_fun_sell_token(ctx: Context<SellPumpToken>, token_amount: u64, slippage: f64, jito_tip_sol: f64) -> Result<()> {
+        instructions::sell_pump_tokens(ctx, token_amount, slippage, jito_tip_sol)
     }
 }
